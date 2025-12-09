373177a33bcdb5f056e10c4f4337039739043a0e
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2025-12-09 17:52:01 +0600

    learning state

--- src/App.js
+++ src/App.js
@@ -4,26 +4,42 @@ import Image from "./components/Image";
 import photo from "./images/0x0.webp"
 
 
+
 // компонент
 class App extends React.Component {
-  helpText = "help text"
+
+  // состояние
+  constructor(props){
+    super(props)
+    this.state = {
+      helpText: "help text"
+    }
+
+    // для взаимодейсвтия метода inputClick с состояниями и менять их
+    this.inputClick = this.inputClick.bind(this)
+  }
+
+
 
   render(){
     return(
       <div>
        <Header title="это свойство" />
        <h1>Hello World</h1>
-       <input placeholder={this.helpText} 
+       <input placeholder={this.state.helpText} 
            onClick={this.inputClick} onMouseEnter={this.mouseOver}  /> 
        <p>{1 + 9}</p>
-       <p>{this.helpText === "help text" ? "yes" : "no"}</p>
+       <p>{this.state.helpText === "help text" ? "yes" : "no"}</p>
        <Image image = {photo} />
      </div> 
      ) 
   }
 
 
-  inputClick() {console.log("clicked")}
+  inputClick() {
+    this.setState({ helpText: "changed"})   // обращается к состоянию с новыям значением
+    console.log("clicked")
+  }
   mouseOver() {console.log("mouse over")}
 
 }
